2adc421b0c2d8c3b745c586621e8c179e45d2455
Muhammad Aminuddin <mhmmdmn18@gmail.com>
2023-06-11 19:27:22 +0700

    first commit

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const { MONGO_URI } = process.env;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } 
   catch (error) {
