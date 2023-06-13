ecd45b3f144179bd779ce686be2052243b9df38e
Muhammad Aminuddin <mhmmdmn18@gmail.com>
2023-06-13 09:42:43 +0700

    update-commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./db');
 const app = express();
-const router = express.Router();
 
 app.use(express.json({}));
 app.use(express.json({
@@ -12,9 +11,6 @@ app.use(express.json({
 dotenv.config();
 connectDB();
 
-router.get('/', async (res) => {
-  res.json('Netweezen! Ready to roll.');
-})
 app.use('/tweets', require('./routes/tweet'));
 
 const PORT = process.env.PORT || 8080;
